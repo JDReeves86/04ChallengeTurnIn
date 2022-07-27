6f3813ca52ec343021ecdb90b2756bd6e368b2f3
JDReeves86 <Reeves.Jacobd@gmail.com>
2022-07-27 09:33:36 -0700

    adding final page

--- Assets/script.js
+++ Assets/script.js
@@ -34,23 +34,22 @@ let quiz = [
     }
 ];
 
-let timeLeft = 30;
-let score = 0; //remove once turning local storage back on.
+let score = ""; //remove once turning local storage back on.
 // let score = localStorage.getItem("score");
 
-scoreBoard.textContent = `Score: --`;
+scoreBoard.textContent = `Score: ${score}` // scoreboard doesn't display properly when placed globally. Functions normally when called locally inside functions.
 
 // provides functionality for start button to begin the quiz. 
 function startQuiz() {
     startButton.style = "visibility: hidden"; // hides start button when quiz started
     quizBody.style = "visibility: visible"; // shows quiz body when quiz started
     hiddenMessage.style = "visibility: hidden";
+    timeLeft = 10;
+    scoreBoard.textContent = `Score: ${score}` // Scoreboard does not reset properly unless reset at the click of starting the quiz.
     let quizTime = setInterval(function() {
         timeLeft--;
         timer.textContent = `Time left: ${timeLeft}`;
         if(timeLeft <= 0){
-            timeLeft = 30;
-            score = 0;
             clearInterval(quizTime);
             outOfTime();
         };
@@ -64,13 +63,14 @@ function outOfTime(){
     quizBody.style = "visibility: hidden";
     startButton.style = "visibility: visible";
     timer.textContent = `You are out of time`;
+    score = 0;
 };
 
 // checks boolean values in object associated with correct answers. True = correct answer. If true, adds a point to the score, if false, timer decreases by 5 seconds.
 function answerChecker(x, y) {
     if (quiz[x].answers[y] == true) {
         score++;
-        scoreBoard.textContent = `Score: ${score}`
+        scoreBoard.textContent = `Score: ${score}` // If taken out of this function, the scoreboard doesn't tally properly.
     }
     else {
         timeLeft-=5;
@@ -101,18 +101,22 @@ function questionTwo() {
     optionA.dataset.quiz = 1;
     optionA.dataset.answer = 0;
     optionA.textContent = quiz[1].choices[0];
+    optionA.removeEventListener("click", questionTwo);
     optionA.addEventListener("click", questionThree);
     optionB.dataset.quiz = 1;
     optionB.dataset.answer = 1;
     optionB.textContent = quiz[1].choices[1];
+    optionB.removeEventListener("click", questionTwo);
     optionB.addEventListener("click", questionThree);
     optionC.dataset.quiz = 1;
     optionC.dataset.answer = 2;
     optionC.textContent = quiz[1].choices[2];
+    optionC.removeEventListener("click", questionTwo);
     optionC.addEventListener("click", questionThree);
     optionD.dataset.quiz = 1;
     optionD.dataset.answer = 3;
     optionD.textContent = quiz[1].choices[3];
+    optionD.removeEventListener("click", questionTwo);
     optionD.addEventListener("click", questionThree);
 };
 function questionThree() {
@@ -120,18 +124,22 @@ function questionThree() {
     optionA.dataset.quiz = 2;
     optionA.dataset.answer = 0;
     optionA.textContent = quiz[2].choices[0];
+    optionA.removeEventListener("click", questionThree);
     optionA.addEventListener("click", questionFour);
     optionB.dataset.quiz = 2;
     optionB.dataset.answer = 1;
     optionB.textContent = quiz[2].choices[1];
+    optionB.removeEventListener("click", questionThree);
     optionB.addEventListener("click", questionFour);
     optionC.dataset.quiz = 2;
     optionC.dataset.answer = 2;
     optionC.textContent = quiz[2].choices[2];
+    optionC.removeEventListener("click", questionThree);
     optionC.addEventListener("click", questionFour);
     optionD.dataset.quiz = 2;
     optionD.dataset.answer = 3;
     optionD.textContent = quiz[2].choices[3];
+    optionD.removeEventListener("click", questionThree);
     optionD.addEventListener("click", questionFour);
 };
 function questionFour() {
@@ -139,18 +147,22 @@ function questionFour() {
     optionA.dataset.quiz = 3;
     optionA.dataset.answer = 0;
     optionA.textContent = quiz[3].choices[0];
+    optionA.removeEventListener("click", questionFour);
     optionA.addEventListener("click", questionFive);
     optionB.dataset.quiz = 3;
     optionB.dataset.answer = 1;
     optionB.textContent = quiz[3].choices[1];
+    optionB.removeEventListener("click", questionFour);
     optionB.addEventListener("click", questionFive);
     optionC.dataset.quiz = 3;
     optionC.dataset.answer = 2;
     optionC.textContent = quiz[3].choices[2];
+    optionC.removeEventListener("click", questionFour);
     optionC.addEventListener("click", questionFive);
     optionD.dataset.quiz = 3;
     optionD.dataset.answer = 3;
     optionD.textContent = quiz[3].choices[3];
+    optionD.removeEventListener("click", questionFour);
     optionD.addEventListener("click", questionFive);
 };
 function questionFive() {
@@ -158,16 +170,32 @@ function questionFive() {
     optionA.dataset.quiz = 4;
     optionA.dataset.answer = 0;
     optionA.textContent = quiz[4].choices[0];
+    optionA.removeEventListener("click", questionFive);
+    optionA.addEventListener("click", questionFive);
     optionB.dataset.quiz = 4;
     optionB.dataset.answer = 1;
     optionB.textContent = quiz[4].choices[1];
+    optionB.removeEventListener("click", questionFive);
+    optionB.addEventListener("click", questionFive);
     optionC.dataset.quiz = 4;
     optionC.dataset.answer = 2;
     optionC.textContent = quiz[4].choices[2];
+    optionC.removeEventListener("click", questionFive);
+    optionC.addEventListener("click", questionFive);
     optionD.dataset.quiz = 4;
     optionD.dataset.answer = 3;
     optionD.textContent = quiz[4].choices[3];
+    optionD.removeEventListener("click", questionFive);
+    optionD.addEventListener("click", questionFive);
 };
+
+function finalPage() {
+    quizBody.remove()
+    let form = document.createElement("form");
+    form.textContent = "Input your name";
+    document.body.appendChild(form)
+}
+
 
 startButton.addEventListener("click", startQuiz);
 startButton.addEventListener("click", questionOne);
